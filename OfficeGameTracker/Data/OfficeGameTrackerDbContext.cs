14b627ed748d297b7e83b4654daba966ca2b496d
usamplazo <petrovicgligorijeTSI10B@gmail.com>
2025-02-05 00:10:41 +0100

    Added DbContext and necessary packages

---
+++ OfficeGameTracker/Data/OfficeGameTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfficeGameTracker.Entities;
+
+namespace OfficeGameTracker.Data
+{
+    public class OfficeGameTrackerDbContext : DbContext
+    {
+        public OfficeGameTrackerDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Contestant> Contestants { get; set; }
+        public DbSet<Discipline> Disciplines { get; set; }
+        public DbSet<Scoreboard> Scoreboards { get; set; }
+
+    }
+}
---
+++ OfficeGameTracker/Services/DatabaseManagmentService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfficeGameTracker.Data;
+
+namespace OfficeGameTracker.Services
+{
+    public class DatabaseManagmentService
+    {
+        public static void MigrationInitialisation(IApplicationBuilder app)
+        {
+            using (IServiceScope serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<OfficeGameTrackerDbContext>().Database.Migrate();
+            }
+        }
+    }
+}
