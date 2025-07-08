8635947bead548acbd1e20867193ea807ed2137c
usamplazo <petrovicgligorijeTSI10B@gmail.com>
2025-07-09 00:10:13 +0200

    Added Contestant crud operations

--- OfficeGameTracker/Controllers/ContestantController.cs
+++ OfficeGameTracker/Controllers/ContestantController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Mvc;
 using OfficeGameTracker.Data;
 using OfficeGameTracker.Entities;
 
@@ -19,6 +20,45 @@ namespace OfficeGameTracker.Controllers
         {
             List<Contestant> contestants = _context.Contestants.ToList();
             return contestants;
+        }
+
+        [HttpGet]
+
+        public Contestant GetContestant(Guid id)
+        {
+            Contestant selectedContestant = _context.Contestants.SingleOrDefault(con => con.ContestantId == id);
+            return selectedContestant;
+        }
+
+        [HttpPut]
+
+        public Contestant AddOrUpdateContestant(Contestant contestant)
+        {
+            Contestant? existingContestant = _context.Contestants.FirstOrDefault(con => con.ContestantId == contestant.ContestantId);
+            if (existingContestant is not null)
+            {
+                existingContestant.Username = contestant.Username;
+                existingContestant.Email = contestant.Email;
+                existingContestant.IsAdmin = contestant.IsAdmin;
+                existingContestant.Age = contestant.Age;
+                existingContestant.Image = contestant.Image;
+                existingContestant.ModifiedOnUtc = DateTime.UtcNow;
+
+                _context.Contestants.Update(existingContestant);
+                return existingContestant;
+            }
+
+            _context.Contestants.Add(contestant);
+            return contestant;
+        }
+
+        [HttpPost]
+        public void RemoveContestant(Guid contestantId)
+        {
+            Contestant? existingContestant = _context.Contestants.FirstOrDefault(con => con.ContestantId == contestantId);
+            _context.Contestants.Remove(existingContestant);
+
+            return;
         }
     }
 }
